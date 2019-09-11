f9a1d0501c6079722673a887091e09902e08e02a
Robie Noor <robienoor@hotmail.co.uk>
2019-09-11 10:29:57 +0100

    with test results

--- learning_loop_artificialdata.py
+++ learning_loop_artificialdata.py
@@ -3,24 +3,27 @@ from keras.layers import Dense
 from keras.models import Sequential
 from keras.utils import to_categorical
 from keras.utils.vis_utils import plot_model
+from sklearn.model_selection import train_test_split
 import random
 
+# Load Data
 X = np.loadtxt(r'C:\Users\Robie\Documents\Programming Learning\Python\learning_loop\dataset\drugLib_raw\artificial_input.txt', delimiter=" ")
 Y = np.loadtxt(r'C:\Users\Robie\Documents\Programming Learning\Python\learning_loop\dataset\drugLib_raw\artificial_labels.txt', delimiter=" ")
 Y_args = np.loadtxt(r'C:\Users\Robie\Documents\Programming Learning\Python\learning_loop\dataset\drugLib_raw\arg_graph_labels.txt', delimiter=" ")
+indices = range(len(X))
+# Break data into train and test
+X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(X,Y_args,indices, test_size=0.05)
 
-test = random.sample(range(1, len(Y_args)), 40)
-
-# c = list(zip(X, Y_args))
-
-# random.shuffle(c)
-
-# X, Y_args = zip(*c)
+print(X_train.shape)
+print(y_train.shape)
+print(X_test.shape)
+print(y_test.shape)
 
 
+# Construct model
 model = Sequential([
-    Dense(100, activation='relu'),
-    Dense(200, activation='relu'),
+    Dense(50, activation='relu'),
+    Dense(50, activation='relu'),
     Dense(3, activation='sigmoid'),
 ])
 
@@ -37,7 +40,7 @@ model.fit(
 )
 
 
-predictions = model.predict(X)
+predictions = model.predict(X_test)
 
 np.savetxt('nn_predictions.csv', predictions,delimiter=",")
-np.savetxt('ratings.csv', Y, delimiter=",")
+np.savetxt('nn_test_indices.csv', indices_test, delimiter=",") 
